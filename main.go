4c4fe26020a98180396b63c49308669420346e60
younhong <younhong@kakao.com>
2021-01-23 16:20:58 +0900

    read and add

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
-
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -21,9 +23,36 @@ type Author struct {
 	LastName  string `json:"lastname"`
 }
 
+// Init Books Variable as a slice Book Struct
+var books []Book
+
 func main() {
 	// Init Router
 	router := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, 
+		Book{
+			ID: "1", 
+			ISBN: "7777", 
+			Title: "First Book", 
+			Author: &Author{
+				FirstName: "Younhong", 
+				LastName: "Ko",
+			},
+		},
+	)
+	books = append(books, 
+		Book{
+			ID: "2", 
+			ISBN: "5555", 
+			Title: "Second Book", 
+			Author: &Author{
+				FirstName: "Sumin", 
+				LastName: "Lee",
+			},
+		},
+	)
 
 	// Route Handlers / Endpoints
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
@@ -37,17 +66,36 @@ func main() {
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get All Books
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	// Loop through book and find by id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)		
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Get All Books
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
 
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(book)
 }
 
 // Get All Books
